850f26664b71393a361411b5f6e6362f83d96639
RC-24 <cullotyross@eircom.net>
2018-03-18 18:14:32 +0000

    Added "Search For Connected Devices" button

--- StravaAutoupload/StravaAutoupload/Form1.Designer.cs
+++ StravaAutoupload/StravaAutoupload/Form1.Designer.cs
@@ -24,11 +24,35 @@
         /// </summary>
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
+            this.btn_SearchForDevices = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_SearchForDevices
+            // 
+            this.btn_SearchForDevices.Location = new System.Drawing.Point(189, 48);
+            this.btn_SearchForDevices.Name = "btn_SearchForDevices";
+            this.btn_SearchForDevices.Size = new System.Drawing.Size(183, 23);
+            this.btn_SearchForDevices.TabIndex = 0;
+            this.btn_SearchForDevices.Text = "Search for connected devices";
+            this.btn_SearchForDevices.UseVisualStyleBackColor = true;
+            this.btn_SearchForDevices.Click += new System.EventHandler(this.btn_SearchForDevices_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 550);
+            this.Controls.Add(this.btn_SearchForDevices);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_SearchForDevices;
     }
 }
 
--- StravaAutoupload/StravaAutoupload/Form1.cs
+++ StravaAutoupload/StravaAutoupload/Form1.cs
@@ -12,6 +12,33 @@ namespace StravaAutoupload {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+
+            ConfigureForm();
+            ConfigureSearchForDevicesButton();
         }
+
+        private void ConfigureForm() {
+            this.Text = "Strava Autoupload";
+            this.Height = 800;
+            this.Width = 600;
+        }
+
+        private void ConfigureSearchForDevicesButton() {
+            btn_SearchForDevices.Text = "Search For Connected Devices";
+            btn_SearchForDevices.Font = new Font("Arial", 10f);
+
+            btn_SearchForDevices.Width = 220;
+            btn_SearchForDevices.Height = 30;
+
+            btn_SearchForDevices.Top = (this.Height / 10);
+            btn_SearchForDevices.Left = (this.Width / 2) - (btn_SearchForDevices.Width / 2);
+        }
+
+        private void btn_SearchForDevices_Click(object sender, EventArgs e) {
+            MessageBox.Show("No devices detected", "Strava Autoupload - Search for Connected Devices", 
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+
     }
 }
