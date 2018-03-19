69030dfc716d7826777941fcb63a5196a46ac8f2
RC-24 <cullotyross@eircom.net>
2018-03-19 19:53:51 +0000

    Prompt user to select which activities to upload

--- StravaAutoupload/StravaAutoupload/FileUtils.cs
+++ StravaAutoupload/StravaAutoupload/FileUtils.cs
@@ -7,9 +7,67 @@ using System.IO;
 
 namespace StravaAutoupload {
     public class FileUtils {
+        private static string PreviouslyUploadedActivitiesFile = "\\PreviouslyUploadedFiles.txt";
+
         public static IList<DriveInfo> GetListOfConnectedDevices() {
             var connectedDevices = DriveInfo.GetDrives().Where(d => d.DriveType == DriveType.Removable && d.IsReady).ToList();
             return connectedDevices;
+        }
+
+        public static IList<FileInfo> GetAllActivitiesFromGarminDevice(DriveInfo device) {
+            // Locate the Activities folder
+            var garminActivitiesFolder = Directory.GetDirectories(device.RootDirectory.ToString(), "Activities", SearchOption.AllDirectories)
+                .First();
+
+            // Get the activities
+            DirectoryInfo activities = new DirectoryInfo(garminActivitiesFolder);
+            return activities.GetFiles("*.fit");
+        }
+
+        public static IList<FileInfo> GetAllNewActivitiesFromGarminDevice(IList<FileInfo> allActivities) {
+            var previouslyUploadedActivities = GetPreviouslyUploadedActivities();
+
+            if(previouslyUploadedActivities.Count == 0) {
+                return allActivities;
+            }
+            else {
+                return GetNewActivities(allActivities, previouslyUploadedActivities);
+            }
+        }
+
+        public static void CreateDirectoryInMyDocuments() {
+            // Create the directory if it doesnt already exist
+            var stravaAutouploadDirectory = Directory.CreateDirectory(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments).ToString() + "\\Strava-Autoupload");
+
+            // Create the PreviouslyUploadedFiles.json file if it doesnt already exist
+            if (!File.Exists(stravaAutouploadDirectory.FullName + PreviouslyUploadedActivitiesFile)) {
+                File.Create(stravaAutouploadDirectory.FullName + PreviouslyUploadedActivitiesFile);
+            }
+        }
+
+        private static IList<String> GetPreviouslyUploadedActivities() {
+            var stravaAutouploadDirectory = Directory.CreateDirectory(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments).ToString() + "\\Strava-Autoupload");
+
+            // Create the PreviouslyUploadedFiles.json file if it doesnt already exist
+            if (!File.Exists(stravaAutouploadDirectory.FullName + PreviouslyUploadedActivitiesFile)) {
+                File.Create(stravaAutouploadDirectory.FullName + PreviouslyUploadedActivitiesFile);
+            }
+
+            return File.ReadAllLines(stravaAutouploadDirectory.FullName + PreviouslyUploadedActivitiesFile).ToList();
+        }
+
+        private static IList<FileInfo> GetNewActivities(IList<FileInfo> allActivities, IList<String> previouslyUploadedActivities) {
+            IList<FileInfo> newActivities = new List<FileInfo>();
+
+            foreach(FileInfo file in allActivities) {
+                if (!previouslyUploadedActivities.Contains(file.Name)) {
+                    newActivities.Add(file);
+                }
+            }
+
+            return newActivities;
         }
     }
 }
---
+++ StravaAutoupload/StravaAutoupload/Form_SelectActivities.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace StravaAutoupload {
+    partial class Form_SelectActivities {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.list_Activities = new System.Windows.Forms.ListView();
+            this.lbl_SelectActivities = new System.Windows.Forms.Label();
+            this.btn_Upload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // list_Activities
+            // 
+            this.list_Activities.Location = new System.Drawing.Point(12, 96);
+            this.list_Activities.Name = "list_Activities";
+            this.list_Activities.Size = new System.Drawing.Size(421, 345);
+            this.list_Activities.TabIndex = 0;
+            this.list_Activities.UseCompatibleStateImageBehavior = false;
+            // 
+            // lbl_SelectActivities
+            // 
+            this.lbl_SelectActivities.AutoSize = true;
+            this.lbl_SelectActivities.Location = new System.Drawing.Point(72, 31);
+            this.lbl_SelectActivities.Name = "lbl_SelectActivities";
+            this.lbl_SelectActivities.Size = new System.Drawing.Size(131, 13);
+            this.lbl_SelectActivities.TabIndex = 1;
+            this.lbl_SelectActivities.Text = "Select Activities to Upload";
+            // 
+            // btn_Upload
+            // 
+            this.btn_Upload.Location = new System.Drawing.Point(171, 447);
+            this.btn_Upload.Name = "btn_Upload";
+            this.btn_Upload.Size = new System.Drawing.Size(75, 23);
+            this.btn_Upload.TabIndex = 2;
+            this.btn_Upload.Text = "Upload Activities";
+            this.btn_Upload.UseVisualStyleBackColor = true;
+            // 
+            // Form_SelectActivities
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 488);
+            this.Controls.Add(this.btn_Upload);
+            this.Controls.Add(this.lbl_SelectActivities);
+            this.Controls.Add(this.list_Activities);
+            this.Name = "Form_SelectActivities";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form_SelectActivities";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView list_Activities;
+        private System.Windows.Forms.Label lbl_SelectActivities;
+        private System.Windows.Forms.Button btn_Upload;
+    }
+}
